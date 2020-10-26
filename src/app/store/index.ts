f1d6259335679f29b0d9cd4a52a94f3ca0cf81db
mehlhorn <mail@nils-mehlhorn.de>
2020-10-26 16:35:00 +0100

    Registering reducers

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,11 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { issueReducer } from './issue/issue.reducer';
+import { IssueState } from './issue/issue.state';
 
-export interface RootState {}
+export interface RootState {
+  issue: IssueState;
+}
 
-export const reducers: ActionReducerMap<RootState> = {};
+export const reducers: ActionReducerMap<RootState> = {
+  issue: issueReducer,
+};
