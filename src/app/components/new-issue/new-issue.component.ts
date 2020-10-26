d88a62fa3f3cf6249c7c292837bfbef5ef19336d
mehlhorn <mail@nils-mehlhorn.de>
2020-10-26 16:07:48 +0100

    Action Creators

---
+++ src/app/components/new-issue/new-issue.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import * as IssueActions from '../../store/issue/issue.actions';
+
+@Component({
+  selector: 'app-new-issue',
+  templateUrl: './new-issue.component.html',
+  styleUrls: ['./new-issue.component.scss'],
+})
+export class NewIssueComponent {
+  form: FormGroup;
+
+  constructor(private store: Store, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      priority: ['low', Validators.required],
+    });
+  }
+
+  submit(): void {
+    const issue = this.form.value;
+    this.store.dispatch(IssueActions.submit(issue));
+  }
+}
