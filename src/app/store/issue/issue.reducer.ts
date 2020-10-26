01b1fac295dd4bb7cd46be391ee7b05e0ef28a22
mehlhorn <mail@nils-mehlhorn.de>
2020-10-26 16:43:08 +0100

    Mutable APIs with immer

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,17 +1,16 @@
 import { createReducer, on } from '@ngrx/store';
 import * as IssueActions from './issue.actions';
 import { initialState } from './issue.state';
+import produce from 'immer';
 
 export const issueReducer = createReducer(
   initialState,
-  on(IssueActions.submit, (state, { issue }) => ({
-    ...state,
-    entities: {
-      ...state.entities,
-      [issue.id]: {
+  on(IssueActions.submit, (state, { issue }) =>
+    produce(state, (draft) => {
+      draft.entities[issue.id] = {
         ...issue,
         resolved: false,
-      },
-    },
-  }))
+      };
+    })
+  )
 );
