543f00f27feafba99a4d28dcad1125a5c521c47f
mehlhorn <mail@nils-mehlhorn.de>
2020-10-28 15:39:20 +0100

    Accessing State from Effects

--- src/app/store/issue/issue.effects.ts
+++ src/app/store/issue/issue.effects.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { filter, map, mergeMap } from 'rxjs/operators';
 import { IssueService } from '../../services/issue.service';
+import { withLatestFromDeferred } from '../../util';
 import * as IssueActions from './issue.actions';
+import * as fromIssue from './issue.selectors';
 
 @Injectable()
 export class IssueEffects {
   submit$ = createEffect(() =>
     this.action$.pipe(
       ofType(IssueActions.submit),
-      mergeMap((action) => this.issues.save(action.issue)),
+      withLatestFromDeferred(this.store.select(fromIssue.selectAll)),
+      filter(([action, issues]) =>
+        issues.every(({ title }) => title !== action.issue.title)
+      ),
+      mergeMap(([action, issues]) => this.issues.save(action.issue)),
       map((issue) => IssueActions.submitSuccess({ issue }))
     )
   );
 
-  constructor(private action$: Actions, private issues: IssueService) {}
+  constructor(
+    private action$: Actions,
+    private issues: IssueService,
+    private store: Store
+  ) {}
 }
--- src/app/util.ts
+++ src/app/util.ts
@@ -1,1 +1,7 @@
+import { Observable, of, pipe } from 'rxjs';
+import { concatMap, withLatestFrom } from 'rxjs/operators';
+
 export const randomId = () => Math.random().toString(36).substr(2, 9);
+
+export const withLatestFromDeferred = <A, B>(other: Observable<B>) =>
+  pipe(concatMap((value: A) => of(value).pipe(withLatestFrom(other))));
