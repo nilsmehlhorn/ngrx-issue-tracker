8c00a545c257befaa54038d8dc8ab519a0dfd8c0
mehlhorn <mail@nils-mehlhorn.de>
2020-10-29 16:01:59 +0100

    Performance

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Issue } from '../../models/issue';
@@ -10,6 +10,7 @@ import * as IssueActions from '../../store/issue/issue.actions';
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
   styleUrls: ['./issue-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssueListComponent {
   issues$: Observable<Issue[]>;
@@ -24,5 +25,9 @@ export class IssueListComponent {
 
   resolve(issue: Issue): void {
     this.store.dispatch(IssueActions.resolve({ issueId: issue.id }));
+  }
+
+  trackByIssues(index: number, issue: Issue): string {
+    return issue.id;
   }
 }
