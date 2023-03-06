87a9e152e688b52e32cc20c0ef6697600241e79b
Nils Mehlhorn <mail@nils-mehlhorn.de>
2023-03-06 13:16:03 +0100

    DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers } from './store';
 import { CounterComponent } from './components/counter/counter.component';
+import { modules } from './modules/modules';
+import { reducers } from './store';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(reducers)],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    modules,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
