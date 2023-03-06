b5a1ef9c7ee55c31a1e24e68c83b44b8f06f6a9d
Nils Mehlhorn <mail@nils-mehlhorn.de>
2023-03-06 14:53:17 +0100

    Action Groups

--- src/app/components/new-issue/new-issue.component.ts
+++ src/app/components/new-issue/new-issue.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import * as IssueActions from '../../store/issue/issue.actions';
+import { IssueActions } from '../../store/issue/issue.actions';
 
 @Component({
   selector: 'app-new-issue',
--- src/app/store/issue/issue.actions.ts
+++ src/app/store/issue/issue.actions.ts
@@ -1,20 +1,17 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup } from '@ngrx/store';
 import { Issue } from '../../models/issue';
 import { randomId } from '../../random-id';
 
-// export const submit = createAction(
-//   '[Issue] Submit',
-//   props<{ issue: Issue }>()
-// );
-
-export const submit = createAction(
-  '[Issue] Submit',
-  (issue: Issue) => {
-    return {
-      issue: {
-        ...issue,
-        id: randomId(),
-      },
-    };
-  }
-);
+export const IssueActions = createActionGroup({
+  source: 'Issues',
+  events: {
+    Submit: (issue: Issue) => {
+      return {
+        issue: {
+          ...issue,
+          id: randomId(),
+        },
+      };
+    },
+  },
+});
