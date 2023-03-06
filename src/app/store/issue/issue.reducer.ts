b27e569ee319a68edfa1e42e426536d78117b4fc
Nils Mehlhorn <mail@nils-mehlhorn.de>
2023-03-06 18:55:27 +0100

    Creating reducers

--- src/app/store/issue/issue.reducer.ts
+++ src/app/store/issue/issue.reducer.ts
@@ -1,4 +1,20 @@
-import { createReducer } from '@ngrx/store';
-import { initialState } from './issue.state';
+import { createReducer, on } from '@ngrx/store';
+import { IssueActions } from './issue.actions';
+import { initialState, IssueState } from './issue.state';
 
-export const issueReducer = createReducer(initialState);
+export const issueReducer = createReducer(
+  initialState,
+  on(
+    IssueActions.submit,
+    (state, { issue }): IssueState => ({
+      ...state,
+      entities: {
+        ...state.entities,
+        [issue.id]: {
+          ...issue,
+          resolved: false,
+        },
+      },
+    })
+  )
+);
