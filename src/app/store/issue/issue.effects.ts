a396e1f7108d4ca7de455fc75956dce36c42df55
Nils Mehlhorn <mail@nils-mehlhorn.de>
2023-03-12 15:14:22 +0100

    Accessing State from Effects

--- src/app/store/issue/issue.effects.ts
+++ src/app/store/issue/issue.effects.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { Actions, createEffect, ofType, concatLatestFrom } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { RootState } from '..';
 import { IssueService } from '../../services/issue.service';
 import { IssueActions } from './issue.actions';
+import * as fromIssue from './issue.selectors';
 
 @Injectable()
 export class IssueEffects {
   submit$ = createEffect(() => {
     return this.action$.pipe(
       ofType(IssueActions.submit),
-      mergeMap((action) => this.issues.save(action.issue)),
+      concatLatestFrom(() => this.store.select(fromIssue.selectAll)),
+      filter(([action, issues]) =>
+        issues.every(({ title }) => title !== action.issue.title)
+      ),
+      mergeMap(([action, issues]) => this.issues.save(action.issue)),
       map((issue) => IssueActions.submitSuccess({ issue }))
     );
   });
 
-  constructor(private action$: Actions, private issues: IssueService) {}
+  constructor(
+    private action$: Actions,
+    private issues: IssueService,
+    private store: Store<RootState>
+  ) {}
 }
