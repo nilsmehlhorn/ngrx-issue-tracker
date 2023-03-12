b3b5ef25276f19a4d5211d415db783f1597baa53
Nils Mehlhorn <mail@nils-mehlhorn.de>
2023-03-12 14:55:48 +0100

    Simulating REST API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IssueDetailComponent } from './components/issue-detail/issue-detail.component';
@@ -11,6 +12,7 @@ import { IssueListComponent } from './components/issue-list/issue-list.component
 import { IssuesComponent } from './components/issues/issues.component';
 import { NewIssueComponent } from './components/new-issue/new-issue.component';
 import { modules } from './modules/modules';
+import { DatabaseService } from './services/database.service';
 import { metaReducers, reducers } from './store';
 
 @NgModule({
@@ -27,6 +29,7 @@ import { metaReducers, reducers } from './store';
     ReactiveFormsModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
+    InMemoryWebApiModule.forRoot(DatabaseService),
     modules,
   ],
   providers: [],
---
+++ src/app/services/database.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Issue } from '../models/issue';
+import { randomId } from '../random-id';
+
+interface Database {
+  issues: Issue[];
+}
+
+@Injectable()
+export class DatabaseService implements InMemoryDbService {
+  createDb(): Database {
+    return {
+      issues: [
+        {
+          id: this.genId(),
+          title: 'Example Issue',
+          description: 'This is a pre-existing issue',
+          priority: 'medium',
+          resolved: false,
+        },
+      ],
+    };
+  }
+
+  genId(): string {
+    return randomId();
+  }
+}
